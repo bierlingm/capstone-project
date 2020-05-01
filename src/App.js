5a3813daf964653d1ad4e50db4212abbf0882f42
Moritz Bierling <bierlingm@gmail.com>
2020-05-01 13:09:05 +0200

    Add basic canvas and some styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './GlobalStyles';
+import styled from 'styled-components/macro'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BodyStyled>
+      <CanvasStyled id="canvas"/>
+    </BodyStyled>
   );
 }
 
-export default App;
+const BodyStyled = styled.div`
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+`
+
+const CanvasStyled = styled.canvas`
+  height: 100vh;
+  width: 100vw;
+`
