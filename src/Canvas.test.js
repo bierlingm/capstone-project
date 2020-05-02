0b90faead2ea4d849b9e3f96fb92995c88951cb9
Moritz Bierling <bierlingm@gmail.com>
2020-05-02 23:39:13 +0200

    Clean up /src and attempt tests (unsuccessfully so far)

---
+++ src/Canvas.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Canvas from './Canvas'
+
+test('shows an editable input field', () => {
+    expect(
+        Canvas
+    )
+    .toBe(
+        <div style={{background: "teal"}}></div>
+    )
+})
