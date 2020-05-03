c84c6ccc4c09d44674a2518cd8869b7cb8a05a38
Moritz Bierling <bierlingm@gmail.com>
2020-05-03 17:48:31 +0200

    Delete GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-// import GlobalStyles from './GlobalStyles'
 import Canvas from './Canvas';
 import WordStream from './WordStream';
 
 export default function App() {
   return (
-    // <GlobalStyles>
-        <Canvas>
-          <WordStream />
-        </Canvas>
-    // </GlobalStyles>
+      <Canvas>
+        <WordStream />
+      </Canvas>
   );
 }
