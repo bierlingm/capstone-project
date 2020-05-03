29367eaee3df455aba33e2024f387e16cf6c6561
Moritz Bierling <bierlingm@gmail.com>
2020-05-03 17:44:40 +0200

    Uncomment the contents of Canvas.js

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,17 +1,17 @@
-// import React from 'react'
-// import styled from 'styled-components'
+import React from 'react'
+import styled from 'styled-components'
 
-// export default function ({children}) {
+export default function ({children}) {
     
-//     return (
-//         <DivStyled>{children}</DivStyled>
-//     )
-// }
+    return (
+        <DivStyled>{children}</DivStyled>
+    )
+}
 
-// const DivStyled = styled.div`
-// height: 100vh;
-// width: 100vw;
-// background: teal;
-// display: grid;
-// grid-template-columns: 50% 50%;
-// `
+const DivStyled = styled.div`
+height: 100vh;
+width: 100vw;
+background: teal;
+display: grid;
+grid-template-columns: 50% 50%;
+`
