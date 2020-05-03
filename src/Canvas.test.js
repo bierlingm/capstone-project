5115885adab86ae2ed8152cfbb7dc85228066b99
Moritz Bierling <bierlingm@gmail.com>
2020-05-03 14:10:53 +0200

    Add App.test.js and play with tests

--- src/Canvas.test.js
+++ src/Canvas.test.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import Canvas from './Canvas'
+// import React from 'react'
+// import Canvas from './Canvas'
 
-test('shows an editable input field', () => {
-    expect(
-        Canvas
-    )
-    .toBe(
-        <div style={{background: "teal"}}></div>
-    )
-})
+// test('shows a monochrome background', () => {
+//     expect(
+//         1+2
+//     ).toBe(
+//         3
+//     )
+// })
