7530d68ac6ccff6c74b50e3139ffa9572375bf67
Moritz Bierling <bierlingm@gmail.com>
2020-05-08 11:11:04 +0200

    Make the whole writing session its own component and import it into the app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
-import { useMachine } from '@xstate/react'
 import React from 'react'
-import styled from 'styled-components'
-import WordStream from './WordStream'
-import { writingSessionMachine } from './WritingSessionMachine'
-import { Canvas, CanvasWriting } from './Canvas'
+import WritingSession from './WritingSession'
 
 export default function App() {
-  const [current, send] = useMachine(writingSessionMachine)
-
-  return current.matches('ready') ? (
-    <Canvas>
-      <ButtonStyled onClick={() => send('START')}>write</ButtonStyled>
-    </Canvas>
-  ) : current.matches('writing') ? (
-    <CanvasWriting>
-      <WordStream />
-    </CanvasWriting>
-  ) : (
-    <Canvas>
-      <ButtonStyled onClick={() => send('START')}>write</ButtonStyled>
-    </Canvas>
-  )
+  return <WritingSession />
 }
-
-const ButtonStyled = styled.button`
-  height: 10%;
-  margin: 0;
-  background: transparent;
-  color: yellow;
-  align-self: center;
-  border: none;
-  padding-left: 24px;
-  font-size: 40px;
-`
---
+++ src/WritingSession.js
@@ -0,0 +1,56 @@
+import { useMachine } from '@xstate/react'
+import React from 'react'
+import styled from 'styled-components'
+import { Canvas, CanvasWriting } from './Canvas'
+import WordStream from './WordStream'
+
+const { Machine } = require('xstate')
+
+const writingSessionMachine = Machine({
+  id: 'writingSession',
+  initial: 'ready',
+  states: {
+    ready: {
+      on: {
+        START: 'writing',
+      },
+    },
+    writing: {
+      on: {
+        STOP: 'ready',
+      },
+      after: {
+        90000: 'ready',
+      },
+    },
+  },
+})
+
+export default function WritingSession() {
+  const [current, send] = useMachine(writingSessionMachine)
+
+  return current.matches('ready') ? (
+    <Canvas>
+      <ButtonStyled onClick={() => send('START')}>write</ButtonStyled>
+    </Canvas>
+  ) : current.matches('writing') ? (
+    <CanvasWriting>
+      <WordStream />
+    </CanvasWriting>
+  ) : (
+    <Canvas>
+      <ButtonStyled onClick={() => send('START')}>write</ButtonStyled>
+    </Canvas>
+  )
+}
+
+const ButtonStyled = styled.button`
+  height: 10%;
+  margin: 0;
+  background: transparent;
+  color: yellow;
+  align-self: center;
+  border: none;
+  padding-left: 24px;
+  font-size: 40px;
+`
