c1e5fa1dd974572a1f89b573966c53311e1286a0
Moritz Bierling <bierlingm@gmail.com>
2020-05-08 12:18:43 +0200

    Move all components into a components folder, update story components and their names

---
+++ src/components/WordStream.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+export default function WordStream() {
+  return <InputStyled autoFocus type="text"></InputStyled>
+}
+
+const InputStyled = styled.input`
+  height: 10%;
+  margin: 0;
+  background: transparent;
+  color: yellow;
+  align-self: center;
+  border: none;
+  padding-left: 24px;
+  font-size: 40px;
+`
---
+++ src/components/WordStream.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import WordStream from './WordStream'
+
+test('shows an editable input field', () => {
+  expect(WordStream()).toBe(<InputStyled type="text"></InputStyled>)
+})
+
+const InputStyled = styled.input`
+  height: 10%;
+  margin: 0;
+  background: transparent;
+  color: yellow;
+  align-self: center;
+  border: none;
+  //  border-right: 2px solid;
+  padding-left: 24px;
+  font-size: 40px;
+`
