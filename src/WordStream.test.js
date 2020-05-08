0a15bb3181afccf30b6f73f84544fe48a7ffb698
Moritz Bierling <bierlingm@gmail.com>
2020-05-08 13:39:08 +0200

    Delete package-lock and node-modules, then npm install again; play with WordStream test

--- src/WordStream.test.js
+++ src/WordStream.test.js
@@ -1,19 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
+import ReactDOM from 'react-dom'
 import WordStream from './WordStream'
 
-test('shows an editable input field', () => {
-  expect(WordStream()).toBe(<InputStyled type="text"></InputStyled>)
+test('input field has focus', () => {
+  ReactDOM.render(<WordStream />)
+  expect(<WordStream />).toHaveFocus()
 })
-
-const InputStyled = styled.input`
-  height: 10%;
-  margin: 0;
-  background: transparent;
-  color: yellow;
-  align-self: center;
-  border: none;
-  //  border-right: 2px solid;
-  padding-left: 24px;
-  font-size: 40px;
-`
